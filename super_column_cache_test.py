fbcae68f42fd9dd85dafe06d6231f560e49b847d
Sylvain Lebresne <sylvain@datastax.com>
2012-04-26 15:04:36 +0200

    Add test for #4190

---
+++ super_column_cache_test.py
@@ -0,0 +1,45 @@
+from dtest import Tester
+from assertions import *
+from tools import *
+
+import os, sys, time, tools
+from uuid import UUID
+from ccmlib.cluster import Cluster
+
+def assert_columns(cli, names):
+    assert not cli.has_errors(), cli.errors()
+    output = cli.last_output()
+
+    for name in names:
+        assert re.search('column=%s' % name, output) is not None, 'Cannot find column %s in %s' % (name, output)
+
+class TestSCCache(Tester):
+
+    def sc_with_row_cache_test(self):
+        """ Test for bug reported in #4190 """
+        cluster = self.cluster
+
+        cluster.populate(1).start()
+        node1 = cluster.nodelist()[0]
+        time.sleep(0.2)
+
+        cli = node1.cli()
+        cli.do("create keyspace ks")
+        cli.do("use ks")
+        cli.do("""
+            create column family Users
+            with column_type='Super' and key_validation_class='UTF8Type' and comparator='UTF8Type' and subcomparator='UTF8Type' and default_validation_class='UTF8Type'
+            and rows_cached=75000 and row_cache_provider='ConcurrentLinkedHashCacheProvider';
+        """)
+
+        cli.do("set Users['mina']['attrs']['name'] = 'Mina'")
+        cli.do("get Users['mina']")
+        assert_columns(cli, ['name'])
+
+        cli.do("set Users['mina']['attrs']['country'] = 'Canada'")
+        cli.do("get Users['mina']")
+        assert_columns(cli, ['name', 'country'])
+
+        cli.do("set Users['mina']['attrs']['region'] = 'Quebec'")
+        cli.do("get Users['mina']")
+        assert_columns(cli, ['name', 'country', 'region'])
