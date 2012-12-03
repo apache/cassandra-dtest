2485c20ca4a87aecb802bf262c28173bc9350bb6
Tyler Patterson <tahooie@gmail.com>
2012-12-03 11:33:35 -0700

    added failing tests list for 1.2

--- failing_tests_1.2.py
+++ failing_tests_1.2.py
@@ -8,13 +8,16 @@ nosetests -ve `./failing_tests.py`
 
 failing_tests = """
     unavailable_schema_test
-    upgrade_to_11_test
+    TestUpgradeTo1_1
     decommission_node_test
-    upgrade_test
-    TestGlobalRowKeyCache
-    rolling_upgrade_test
+    all_all_test
+    TestCounters.upgrade_test
+    non_local_read_test
+    TestRollingUpgrade
     incompressible_data_in_compressed_table_test
-    short_read_test
+    upgrade_through_versions_test
+    cql3_insert_thrift_test
+
 """
 
 lines = (l.strip() for l in failing_tests.splitlines() if l.strip())
