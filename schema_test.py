5ea6bd85cade750cf011b8cd707cbecaa00958dd
Aleksey Yeschenko <aleksey@yeschenko.com>
2013-04-18 23:24:53 +0300

    add compaction test for #3919

--- schema_test.py
+++ schema_test.py
@@ -17,7 +17,42 @@ class TestSchema(Tester):
                          cm.exception.message)
 
     @since('#3919')
-    def drop_column_test(self):
+    def drop_column_compaction_test(self):
+        cursor = self.prepare()
+        cursor.execute("USE ks")
+        cursor.execute("CREATE TABLE cf (key int PRIMARY KEY, c1 int, c2 int)")
+
+        # insert some data.
+        cursor.execute("INSERT INTO cf (key, c1, c2) VALUES (0, 1, 2)")
+        cursor.execute("INSERT INTO cf (key, c1, c2) VALUES (1, 2, 3)")
+        cursor.execute("INSERT INTO cf (key, c1, c2) VALUES (2, 3, 4)")
+
+        # drop and readd c1.
+        cursor.execute("ALTER TABLE cf DROP c1")
+        cursor.execute("ALTER TABLE cf ADD c1 int")
+
+        # add another row.
+        cursor.execute("INSERT INTO cf (key, c1, c2) VALUES (3, 4, 5)")
+
+        node = self.cluster.nodelist()[0]
+        node.flush()
+        node.compact()
+
+        # erase info on dropped 'c1' column and restart.
+        cursor.execute("""UPDATE system.schema_columnfamilies
+                          SET dropped_columns = null
+                          WHERE keyspace_name = 'ks' AND columnfamily_name = 'cf'""")
+        node.stop(gently=False)
+        node.start()
+        time.sleep(.5)
+
+        # test that c1 values have been compacted away.
+        cursor = self.cql_connection(node, version='3.0.10').cursor()
+        cursor.execute("SELECT c1 FROM ks.cf")
+        self.assertEqual([[None], [None], [None], [4]], sorted(cursor.fetchall()))
+
+    @since('#3919')
+    def drop_column_queries_test(self):
         cursor = self.prepare()
 
         cursor.execute("USE ks")
@@ -58,7 +93,7 @@ class TestSchema(Tester):
     def prepare(self):
         cluster = self.cluster
         cluster.populate(1).start()
-        time.sleep(1)
+        time.sleep(.5)
         nodes = cluster.nodelist()
         cursor = self.cql_connection(nodes[0], version='3.0.10').cursor()
         self.create_ks(cursor, 'ks', 1)
