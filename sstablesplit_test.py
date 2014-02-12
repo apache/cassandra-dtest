b7bfcde7b2f9573178a16e314daefbbbc961dc2d
Michael Shuler <michael@pbandjelly.org>
2014-02-12 15:49:44 -0600

    Write 1.5m iterations, grab/pass version to sstablesplit
    - Succeeds on first pass, but ends with 9 split sstables second pass :(

--- sstablesplit_test.py
+++ sstablesplit_test.py
@@ -16,34 +16,38 @@ class TestCounters(Tester):
         cluster = self.cluster
         cluster.populate(1).start()
         node = cluster.nodelist()[0]
+        version = cluster.version()
         debug("Run stress to insert data")
-        if cluster.version() < "2.1":
+        if version < "2.1":
             node.stress( ['-o', 'insert'] )
         else:
-            node.stress( ['write', 'n=1000000', '-rate', 'threads=8'] )
+            node.stress( ['write', 'n=1500000', '-rate', 'threads=8'] )
         
         self._do_compaction(node)
-        self._do_split(node)
+        self._do_split(node, version)
         self._do_compaction(node)
-        self._do_split(node)
+        self._do_split(node, version)
 
         debug("Run stress to ensure data is readable")
-        if cluster.version() < "2.1":
+        if version < "2.1":
             node.stress( ['-o', 'read'] )
         else:
-            node.stress( ['read', 'n=1000000', '-rate', 'threads=8'] )
+            node.stress( ['read', 'n=1500000', '-rate', 'threads=8'] )
 
     def _do_compaction(self, node):
         debug("Compact sstables.")
         node.compact()
         sstables = node.get_sstables('Keyspace1', '')
 
-    def _do_split(self, node):
+    def _do_split(self, node, version):
         debug("run sstablesplit")
         time.sleep(5.0)
         node.stop()
         node.run_sstablesplit( keyspace='Keyspace1' )
         sstables = node.get_sstables('Keyspace1', '')
         assert len(sstables) == 6, "Incorrect number of sstables after running sstablesplit."
-        assert max( [ getsize( sstable ) for sstable in sstables ] ) <= 52428960, "Max sstables size should be 52428960."
+        if version < "2.1":
+            assert max( [ getsize( sstable ) for sstable in sstables ] ) <= 52428960, "Max sstables size should be 52428960."
+        else:
+            assert max( [ getsize( sstable ) for sstable in sstables ] ) <= 52428980, "Max sstables size should be 52428980."
         node.start()
