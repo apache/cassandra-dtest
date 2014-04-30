9c8eb9ea44ae628ac37d75dcaa9f42aab845568e
Shawn Kumar <shawn.kumat@datastax.com>
2014-04-30 15:01:19 -0700

    added tests for replacing active/nonexistent nodes

--- replace_address_test.py
+++ replace_address_test.py
@@ -2,6 +2,7 @@ from dtest import Tester, debug, DISABLE_VNODES
 import unittest
 from tools import *
 from ccmlib.cluster import Cluster
+from ccmlib.node import NodeError
 import time
 from cql import OperationalError
 from cql.cassandra.ttypes import UnavailableException
@@ -18,10 +19,12 @@ class TestReplaceAddress(Tester):
             # node that hasn't started yet, and when it does, it gets
             # replayed and everything is fine.
             r'Can\'t send migration request: node.*is down',
+            r'Exception encountered during startup',
+            r'Exception in thread Thread'
         ]
         Tester.__init__(self, *args, **kwargs)
 
-    def replace_node_test(self):
+    def replace_stopped_node_test(self):
         """Check that the replace address function correctly replaces a node that has failed in a cluster. 
         Create a cluster, cause a node to fail, and bring up a new node with the replace_address parameter.
         Check that tokens are migrated and that data is replicated properly.
@@ -70,9 +73,52 @@ class TestReplaceAddress(Tester):
             self.assertEqual(len(movedTokensList), 256)
 
         #check that restarting node 3 doesn't work
-        
         debug("Try to restart node 3 (should fail)")
         node3.start() 
         checkCollision = node1.grep_log("between /127.0.0.3 and /127.0.0.4; /127.0.0.4 is the new owner")
         debug(checkCollision)
         self.assertEqual(len(checkCollision), 1)
+
+    def replace_active_node_test(self):
+
+        debug("Starting cluster with 3 nodes.")
+        cluster = self.cluster
+        cluster.populate(3).start()
+        [node1,node2, node3] = cluster.nodelist()
+
+        debug("Inserting Data...")
+        node1.stress(['write', 'n=10000', '-schema', 'replication(factor=3)'])
+        cursor = self.patient_cql_connection(node1).cursor()
+        cursor.execute('select * from "Keyspace1"."Standard1" LIMIT 1', consistency_level='THREE')
+        initialData = cursor.fetchall()
+
+        #replace active node 3 with node 4
+        debug("Starting node 4 to replace active node 3")
+        node4 = Node('node4', cluster, True, ('127.0.0.4', 9160), ('127.0.0.4', 7000), '7400', '0', None, ('127.0.0.4',9042))
+        cluster.add(node4, False)
+        
+        with self.assertRaises(NodeError):
+            node4.start(replace_address='127.0.0.3')
+
+        checkError = node4.grep_log("java.lang.UnsupportedOperationException: Cannnot replace a live node...")
+
+        self.assertEqual(len(checkError), 1)
+
+    def replace_nonexistent_node_test(self):
+        debug("Starting cluster with 3 nodes.")
+        cluster = self.cluster
+        cluster.populate(3).start()
+        [node1,node2, node3] = cluster.nodelist()
+
+        debug("Inserting Data...")
+        node1.stress(['write', 'n=10000', '-schema', 'replication(factor=3)'])
+        cursor = self.patient_cql_connection(node1).cursor()
+        cursor.execute('select * from "Keyspace1"."Standard1" LIMIT 1', consistency_level='THREE')
+        initialData = cursor.fetchall()
+
+        debug('Start node 4 and replace an address with no node')
+        node4 = Node('node4', cluster, True, ('127.0.0.4', 9160), ('127.0.0.4', 7000), '7400', '0', None, ('127.0.0.4',9042))
+        cluster.add(node4, False)
+        
+        with self.assertRaises(NodeError):
+            node4.start(replace_address='127.0.0.5')
