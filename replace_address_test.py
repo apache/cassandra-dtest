c788751eec7840d658be374bcf3054c56ff80a2e
Shawn Kumar <shawn.kumat@datastax.com>
2014-04-30 13:29:38 -0700

    handle vnods, added debug stmts

--- replace_address_test.py
+++ replace_address_test.py
@@ -1,4 +1,4 @@
-from dtest import Tester, debug
+from dtest import Tester, debug, DISABLE_VNODES
 import unittest
 from tools import *
 from ccmlib.cluster import Cluster
@@ -26,19 +26,23 @@ class TestReplaceAddress(Tester):
         Create a cluster, cause a node to fail, and bring up a new node with the replace_address parameter.
         Check that tokens are migrated and that data is replicated properly.
         """
-
+        debug("Starting cluster with 3 nodes.")
         cluster = self.cluster
         cluster.populate(3).start()
         [node1,node2, node3] = cluster.nodelist()
 
+        debug("Inserting Data...")
         node1.stress(['write', 'n=10000', '-schema', 'replication(factor=3)'])
         cursor = self.patient_cql_connection(node1).cursor()
         cursor.execute('select * from "Keyspace1"."Standard1" LIMIT 1', consistency_level='THREE')
         initialData = cursor.fetchall()
         
         #stop node, query should not work with consistency 3
+        debug("Stopping node 3.")
         node3.stop(gently=False)
         time.sleep(5)
+
+        debug("Testing node stoppage (query should fail).")
         with self.assertRaises(NodeUnavailable):
             try:
                 cursor.execute('select * from "Keyspace1"."Standard1" LIMIT 1', consistency_level='THREE')
@@ -46,25 +50,29 @@ class TestReplaceAddress(Tester):
                 raise NodeUnavailable("Node could not be queried.")
 
         #replace node 3 with node 4
+        debug("Starting node 4 to replace node 3")
         node4 = Node('node4', cluster, True, ('127.0.0.4', 9160), ('127.0.0.4', 7000), '7400', '0', None, ('127.0.0.4',9042))
         cluster.add(node4, False)
         node4.start(replace_address='127.0.0.3')
 
         #query should work again
+        debug("Verifying querying works again.")
         cursor.execute('select * from "Keyspace1"."Standard1" LIMIT 1', consistency_level='THREE')
         finalData = cursor.fetchall()
         self.assertListEqual(initialData, finalData)
         
-        movedTokensList = node4.grep_log("changing ownership from /127.0.0.3 to /127.0.0.4")
+        debug("Verifying tokens migrated sucessfully")
+        movedTokensList = node4.grep_log("Token .* changing ownership from /127.0.0.3 to /127.0.0.4")
         debug(movedTokensList[0])
-        self.assertGreaterEqual(len(movedTokensList), 1)
+        if DISABLE_VNODES:
+            self.assertEqual(len(movedTokensList), 1)
+        else:
+            self.assertEqual(len(movedTokensList), 256)
 
         #check that restarting node 3 doesn't work
+        
+        debug("Try to restart node 3 (should fail)")
         node3.start() 
         checkCollision = node1.grep_log("between /127.0.0.3 and /127.0.0.4; /127.0.0.4 is the new owner")
         debug(checkCollision)
-        self.assertGreaterEqual(len(checkCollision), 1)
-
-        updatedSeedList = node4.grep_log("FatClient /127.0.0.3 has been silent for 30000ms, removing from gossip")
-        debug(updatedSeedList)
-        self.assertGreaterEqual(len(updatedSeedList), 1)
+        self.assertEqual(len(checkCollision), 1)
