ebd30aeb2b53616ae5b08c81063342393567b389
Philip Thompson <philip.thompson@datastax.com>
2014-07-09 11:53:34 -0400

    Added replace_in_file to pytools

--- pytools.py
+++ pytools.py
@@ -137,6 +137,21 @@ def range_putget(cluster, cursor, cl=ConsistencyLevel.QUORUM):
         res = cursor.fetchmany(100)
         _validate_row(cluster, res)
 
+def replace_in_file(filepath, search_replacements):
+    """In-place file search and replace.
+    
+    filepath - The path of the file to edit 
+    search_replacements - a list of tuples (regex, replacement) that
+    represent however many search and replace operations you wish to
+    perform.
+
+    Note: This does not work with multi-line regexes.
+    """
+    for line in fileinput.input(filepath, inplace=True):
+        for regex, replacement in search_replacements:
+            line = re.sub(regex, replacement, line)
+        sys.stdout.write(line)
+
 class since(object):
     def __init__(self, cass_version, max_version=None):
         self.cass_version = LooseVersion(cass_version)
