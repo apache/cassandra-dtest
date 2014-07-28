2736e68739f917a11581f34ee5816319ab94c239
Philip Thompson <philip.thompson@datastax.com>
2014-07-28 17:36:07 -0500

    1.1 now uses sstables to load
    This will allow us to use the python driver,
    and avoiding loading data via thrift

--- upgrade_through_versions_test.py
+++ upgrade_through_versions_test.py
@@ -4,6 +4,7 @@ from distutils.version import LooseVersion
 from cql import OperationalError
 from dtest import Tester, debug, DISABLE_VNODES, DEFAULT_DIR
 from tools import new_node, not_implemented
+from ccmlib import common as ccmcommon
 
 TRUNK_VER = '2.2'
 
@@ -140,8 +141,7 @@ class TestUpgradeThroughVersions(Tester):
             if mixed_version:
                 for num, node in enumerate(self.cluster.nodelist()):
                     # do a write and check for each new node as upgraded
-                    self._write_values()
-                    self._increment_counters()
+                    self._prepare_for_upgrade()
                     
                     self.upgrade_to_version(tag, mixed_version=True, nodes=(node,))
                     
@@ -151,8 +151,7 @@ class TestUpgradeThroughVersions(Tester):
                     debug('Successfully upgraded %d of %d nodes to %s' % 
                           (num+1, len(self.cluster.nodelist()), tag))
             else:
-                self._write_values()
-                self._increment_counters()
+                self._prepare_for_upgrade()
                 
                 self.upgrade_to_version(tag)
                 
@@ -232,6 +231,40 @@ class TestUpgradeThroughVersions(Tester):
                 c counter,
                 PRIMARY KEY (k1, k2)
                 );""")
+
+    def _prepare_for_upgrade(self):
+        if self.cluster.version() >= '1.2':
+            self._write_values()
+            self._increment_counters()
+        else:
+            node1 = self.cluster.nodelist()[0]
+            cdir = node1.get_cassandra_dir()
+            sstableloader = os.path.join(cdir, 'bin', 'sstableloader')
+            env = ccmcommon.make_cassandra_env(cdir, node1.get_path())
+            host = node1.address()
+            sstablecopy_dir = os.path.join(os.getcwd(), 'upgrade')
+            if not os.path.isdir(sstablecopy_dir):
+                cmd_args = ['tar', '-xvzf', 'oneonesstables.tar.gz', 'upgrade/']
+                p = subprocess.Popen(cmd_args)
+                exit_status = p.wait()
+                self.assertEqual(0, exit_status, "tarball was unzipped successfully.")
+
+            for cf_dir in os.listdir(sstablecopy_dir):
+                full_cf_dir = os.path.join(sstablecopy_dir, cf_dir)
+                if os.path.isdir(full_cf_dir):
+                    cmd_args = [sstableloader, '--nodes', host, full_cf_dir]
+                    p = subprocess.Popen(cmd_args, env=env)
+                    exit_status = p.wait()
+                    self.assertEqual(0, exit_status,
+                        "sstableloader exited with a non-zero status: %d" % exit_status)
+
+            for i in xrange(100):
+                x = len(self.row_values) + 1
+                self.row_values.add(x)
+
+            self.expected_counts = {}
+            for i in range(10):
+                self.expected_counts[uuid.uuid4()] = defaultdict(int)
 
     def _write_values(self, num=100):
         cursor = self.patient_cql_connection(self.node2).cursor()
