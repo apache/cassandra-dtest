61f3f1d9d85cd94a098f23fb3363b5ee2b719357
Philip Thompson <philip.thompson@datastax.com>
2014-08-04 09:52:42 -0500

    Added new consistency tests
    Using CL.TWO and CL.THREE. A small increase
    in coverage for CASSANDRA-5460

--- consistency_test.py
+++ consistency_test.py
@@ -28,7 +28,7 @@ class TestConsistency(Tester):
             query_c1c2(cursor2, n, ConsistencyLevel.QUORUM)
 
 
-        # shutdown a node an test again
+        # shutdown a node and test again
         node3.stop(wait_other_notice=True)
         for n in xrange(100, 200):
             insert_c1c2(session, n, ConsistencyLevel.QUORUM)
@@ -76,13 +76,13 @@ class TestConsistency(Tester):
             insert_c1c2(cursor1, n, ConsistencyLevel.ONE)
             retry_till_success(query_c1c2, cursor2, n, ConsistencyLevel.ONE, timeout=5)
 
-        # shutdown a node an test again
+        # shutdown a node and test again
         node3.stop(wait_other_notice=True)
         for n in xrange(100, 200):
             insert_c1c2(cursor1, n, ConsistencyLevel.ONE)
             retry_till_success(query_c1c2, cursor2, n, ConsistencyLevel.ONE, timeout=5)
 
-        # shutdown a second node an test again
+        # shutdown a second node and test again
         node2.stop(wait_other_notice=True)
         for n in xrange(200, 300):
             insert_c1c2(cursor1, n, ConsistencyLevel.ONE)
@@ -105,7 +105,7 @@ class TestConsistency(Tester):
             insert_c1c2(cursor1, n, ConsistencyLevel.ONE)
             query_c1c2(cursor2, n, ConsistencyLevel.ALL)
 
-        # shutdown a node an test again
+        # shutdown a node and test again
         node3.stop(wait_other_notice=True)
         insert_c1c2(cursor1, 100, ConsistencyLevel.ONE)
         assert_unavailable(query_c1c2, cursor2, 100, ConsistencyLevel.ALL)
@@ -127,9 +127,50 @@ class TestConsistency(Tester):
             insert_c1c2(cursor1, n, ConsistencyLevel.ALL)
             query_c1c2(cursor2, n, ConsistencyLevel.ONE)
 
-        # shutdown a node an test again
+        # shutdown a node and test again
         node3.stop(wait_other_notice=True)
         assert_unavailable(insert_c1c2, cursor1, 100, ConsistencyLevel.ALL)
+
+    def quorum_two_test(self):
+        cluster = self.cluster
+
+        cluster.populate(3).start()
+        [node1, node2, node3] = cluster.nodelist()
+
+        cursor1 = self.patient_cql_connection(node1)
+        self.create_ks(cursor1, 'ks', 3)
+        create_c1c2_table(self, cursor1)
+
+        cursor2 = self.cql_connection(node2, 'ks')
+
+        for n in xrange(0, 100):
+            insert_c1c2(cursor1, n, ConsistencyLevel.QUORUM)
+            query_c1c2(cursor2, n, ConsistencyLevel.TWO)
+
+        # shutdown a node and test again
+        node3.stop(wait_other_notice=True)
+        for n in xrange(0, 100):
+            query_c1c2(cursor2, n, ConsistencyLevel.TWO)
+
+    def quorum_three_test(self):
+        cluster = self.cluster
+
+        cluster.populate(3).start()
+        [node1, node2, node3] = cluster.nodelist()
+
+        cursor1 = self.patient_cql_connection(node1)
+        self.create_ks(cursor1, 'ks', 3)
+        create_c1c2_table(self, cursor1)
+
+        cursor2 = self.cql_connection(node2, 'ks')
+
+        for n in xrange(0, 100):
+            insert_c1c2(cursor1, n, ConsistencyLevel.QUORUM)
+            query_c1c2(cursor2, n, ConsistencyLevel.THREE)
+
+        # shutdown a node and test again
+        node3.stop(wait_other_notice=True)
+        assert_unavailable(insert_c1c2, cursor1, 100, ConsistencyLevel.THREE)
 
     def short_read_test(self):
         cluster = self.cluster
