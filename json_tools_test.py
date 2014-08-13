009f5dac01a2a010195fe8ca428be1a0c21ac761
Philip Thompson <philip.thompson@datastax.com>
2014-08-13 09:47:25 -0500

    Ported json_tools to pydriver

--- json_tools_test.py
+++ json_tools_test.py
@@ -1,4 +1,5 @@
-from dtest import Tester, debug
+from dtest import PyTester as Tester, debug
+from pytools import rows_to_list
 import tempfile
 import os
 
@@ -14,7 +15,7 @@ class TestJson(Tester):
 
         debug("Getting CQLSH...")
         [node1] = cluster.nodelist()
-        cursor = self.patient_cql_connection(node1).cursor()
+        cursor = self.patient_cql_connection(node1)
 
         debug("Inserting data...")
         self.create_ks(cursor, 'Test', 1)
@@ -32,10 +33,9 @@ class TestJson(Tester):
         cursor.execute("INSERT INTO Test. users (user_name, password, gender, state, birth_year) VALUES('frodo', 'pass@', 'male', 'CA', 1985);")
         cursor.execute("INSERT INTO Test. users (user_name, password, gender, state, birth_year) VALUES('sam', '@pass', 'male', 'NY', 1980);")
 
-        cursor.execute("SELECT * FROM Test. users")
-        res = cursor.fetchall()
+        res = cursor.execute("SELECT * FROM Test. users")
 
-        self.assertItemsEqual(res,
+        self.assertItemsEqual(rows_to_list(res),
            [ [ u'frodo', 1985, u'male', u'pass@', u'CA' ],
               [u'sam', 1980, u'male', u'@pass', u'NY' ] ] )
 
@@ -53,7 +53,7 @@ class TestJson(Tester):
         cluster.start()
 
         debug("Inserting data...")
-        cursor = self.patient_cql_connection(node1).cursor()
+        cursor = self.patient_cql_connection(node1)
         self.create_ks(cursor, 'Test', 1)
 
         cursor.execute("""
@@ -78,14 +78,13 @@ class TestJson(Tester):
         debug("Verifying import...")
         cluster.start()
         [node1] = cluster.nodelist()
-        cursor = self.patient_cql_connection(node1).cursor()
+        cursor = self.patient_cql_connection(node1)
 
-        cursor.execute("SELECT * FROM Test. users")
-        res = cursor.fetchall()
+        res = cursor.execute("SELECT * FROM Test. users")
 
         debug("data: " + str(res))
 
-        self.assertItemsEqual(res,
+        self.assertItemsEqual(rows_to_list(res),
            [ [ u'frodo', 1985, u'male', u'pass@', u'CA' ],
                 [u'sam', 1980, u'male', u'@pass', u'NY' ],
                 [u'gandalf', 1955, u'male', u'p@$$', u'WA'] ] )
