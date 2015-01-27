cbf490cdffd2db7f032994723f2c1be3aa8d57bd
Shawn Kumar <shawn.kumar@datastax.com>
2015-01-27 09:04:14 -0800

    coverage for 8613 and updated inc repair

--- repair_compaction_test.py
+++ repair_compaction_test.py
@@ -2,19 +2,53 @@ from dtest import Tester, debug
 from tools import insert_c1c2, since
 from cassandra import ConsistencyLevel
 from ccmlib.node import Node
+from re import search, findall
 import unittest
+import time
+import os
+from assertions import assert_invalid, assert_one, assert_all, assert_none
 
 class TestRepairCompaction(Tester):
 
-    @since('2.1')
-    def repair_compaction_fine_test(self):
-        """Check that we do not stream data for repairs past the last repair.
-        Check cases i) node goes down, data inserted, node comes up run repair
-        ii) after case i, do similar with a different node down (since compaction has seperated repaired)
-        iii) check that repair works appropriately where a new node is replacing
-        """
+    def __init__(self, *args, **kwargs):
+        # Ignore these log patterns:
+        self.ignore_log_patterns = [
+            r'Can\'t send migration request: node.*is down',
+        ]
+        Tester.__init__(self, *args, **kwargs)
+
+    @since('3.0')
+    def sstable_marking_test(self):
         cluster = self.cluster
-        cluster.set_configuration_options(values={ 'hinted_handoff_enabled' : False, 'auto_snapshot': False}, batch_commitlog=False)
+        cluster.populate(3).start()
+        [node1,node2,node3] = cluster.nodelist()
+
+        node3.stop(gently=True)
+
+        node1.stress(['write', 'n=10000', '-schema', 'replication(factor=3)'])
+        node1.flush()
+        node2.flush()
+
+        node3.start(wait_other_notice=True)
+        time.sleep(3)
+        node3.repair()
+
+        output = ""
+        with open('sstables.txt', 'w') as f:
+            node1.run_sstablemetadata(output_file=f, keyspace='keyspace1')
+            node2.run_sstablemetadata(output_file=f, keyspace='keyspace1')
+            node3.run_sstablemetadata(output_file=f, keyspace='keyspace1')
+
+        with open("sstables.txt", 'r') as r:
+            output = r.read().replace('\n', '')
+        
+        self.assertNotIn('repairedAt: 0', output)
+
+        os.remove('sstables.txt')
+
+    @since('3.0')
+    def multiple_repair_test(self):
+        cluster = self.cluster
         cluster.populate(3).start()
         [node1, node2, node3] = cluster.nodelist()
 
@@ -22,9 +56,9 @@ class TestRepairCompaction(Tester):
         self.create_ks(cursor, 'ks', 3)
         self.create_cf(cursor, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
 
-        debug("insert data into all")
+        debug("insert data")
 
-        for x in range(1, 5):
+        for x in range(1, 50):
             insert_c1c2(cursor, x, ConsistencyLevel.ALL)
         node1.flush()
 
@@ -33,7 +67,7 @@ class TestRepairCompaction(Tester):
         node3.stop(gently=False)
 
         debug("inserting additional data into node 1 and 2")
-        for y in range(5, 10):
+        for y in range(50, 100):
             insert_c1c2(cursor, y, ConsistencyLevel.TWO)
         node1.flush()
         node2.flush()
@@ -42,18 +76,11 @@ class TestRepairCompaction(Tester):
         node3.start()
         node3.repair()
 
-        sstableNode1 = node3.grep_log("reading file from /127.0.0.1, repairedAt = 0")
-        sstableNode2 = node3.grep_log("reading file from /127.0.0.2, repairedAt = 0")
-        catchBadSSReads = node3.grep_log("reading file from .* repairedAt = ([1-9])")
-        self.assertGreaterEqual(len(sstableNode1), 1)
-        self.assertGreaterEqual(len(sstableNode2), 1)
-        self.assertLess(len(catchBadSSReads), 1)
-
         debug("stopping node 2")
         node2.stop(gently=False)
 
         debug("inserting data in nodes 1 and 3")
-        for z in range(10, 15):
+        for z in range(100, 150):
             insert_c1c2(cursor, z, ConsistencyLevel.TWO)
         node1.flush()
 
@@ -62,31 +89,104 @@ class TestRepairCompaction(Tester):
         node2.start()
         node2.repair()
 
-        fileFromNode1 = node2.grep_log("reading file from /127.0.0.1, repairedAt = 0")
-        fileFromNode3 = node2.grep_log("reading file from /127.0.0.3, repairedAt = 0")
-        catchBadReads = node2.grep_log("reading file from .* repairedAt = ([1-9])")
-        self.assertGreaterEqual(len(fileFromNode1), 1)
-        self.assertGreaterEqual(len(fileFromNode3), 1)
-        self.assertLess(len(catchBadReads), 1)
-
-        node4 = Node('node4', cluster, True, ('127.0.0.4', 9160), ('127.0.0.4', 7000), '7400', '0', None, ('127.0.0.4',9042))
-        node4.start()
-
-        debug("replace node and check repair-like process")
+        debug("replace node and check data integrity")
         node3.stop(gently=False)
         node5 = Node('node5', cluster, True, ('127.0.0.5', 9160), ('127.0.0.5', 7000), '7500', '0', None, ('127.0.0.5',9042))
         cluster.add(node5, False)
         node5.start(replace_address = '127.0.0.3', wait_other_notice=True)
 
-        fileRead = node5.grep_log("reading file from .*, repairedAt = 0")
-        self.assertGreaterEqual(len(fileRead), 1)
+        assert_one(cursor, "SELECT COUNT(*) FROM ks.cf LIMIT 100", [14])
 
-        # additionally should see 14 distinct keys in data(this prints to command line)
-        debug((node2.run_sstable2json()))
+    @since('3.0')
+    def sstable_repairedset_test(self):
+        cluster = self.cluster
+        cluster.populate(2).start()
+        [node1,node2] = cluster.nodelist()
+        node1.stress(['write', 'n=10000', '-schema', 'replication(factor=2)'])
 
-        rows = cursor.execute("SELECT COUNT(*) FROM ks.cf LIMIT 100")
+        node1.flush()
+        node2.flush()
 
-        results = rows[0]
-        debug(results)
+        node2.stop(gently=False)
 
-        self.assertEqual(results[0], 14)
+        node2.run_sstablerepairedset(keyspace='keyspace1')
+        node2.start()
+
+        with open('initial.txt', 'w') as f:
+            node2.run_sstablemetadata(output_file=f, keyspace='keyspace1')
+            node1.run_sstablemetadata(output_file=f, keyspace='keyspace1')
+
+        with open('initial.txt', 'r') as g:
+            initialoutput = g.read()
+
+        node1.stop()
+        node2.stress(['write', 'n=10000', '-schema', 'replication(factor=2)'])
+        node2.flush()
+        node1.start()
+        node1.repair()
+
+        with open('final.txt', 'w') as h:
+            node1.run_sstablemetadata(output_file=h, keyspace='keyspace1')
+            node2.run_sstablemetadata(output_file=h, keyspace='keyspace1')
+
+        with open('final.txt', 'r') as r:
+            finaloutput = r.read()
+
+        matches = findall('(?<=Repaired at:).*', finaloutput)
+
+        debug(matches)
+
+        uniquematches = []
+        matchcount = []
+        for value in matches:
+            if value not in uniquematches:
+                uniquematches.append(value)
+                matchcount.append(1)
+            else:
+                index = uniquematches.index(value)
+                matchcount[index] = matchcount[index] + 1
+
+        self.assertEqual(len(uniquematches), 3)
+
+        self.assertEqual(max(matchcount), 3)
+
+        self.assertNotIn('repairedAt: 0', finaloutput)
+
+        os.remove('initial.txt')
+        os.remove('final.txt')
+    
+    @since('3.0')
+    def compaction_test(self):
+        cluster = self.cluster
+        cluster.populate(3).start()
+        [node1,node2,node3] = cluster.nodelist()
+
+        cursor = self.patient_cql_connection(node1)
+        self.create_ks(cursor, 'ks', 3)
+        cursor.execute("create table tab(key int PRIMARY KEY, val int);")
+
+        node3.stop()
+
+        for x in range(0, 100):
+            cursor.execute("insert into tab(key,val) values(" + str(x) + ",0)")
+        node1.flush()
+
+        node3.start()
+
+        node3.repair()
+
+        for x in range(0, 150):
+            cursor.execute("insert into tab(key,val) values(" + str(x) + ",1)")
+        node1.flush()
+        node2.flush()
+        node3.flush()
+
+        node3.nodetool('compact')
+
+        for x in range(0, 150):
+            assert_one(cursor, "select val from tab where key =" + str(x), [1])
+
+
+
+
+
