e9f4f2ae5ab6585b246668628b0f0eed332b76b4
Alan Boudreault <alan@alanb.ca>
2015-01-31 06:37:56 -0500

    Fix error message of assert_one to report the correct expected value

--- assertions.py
+++ assertions.py
@@ -31,7 +31,7 @@ def assert_one(cursor, query, expected, cl=ConsistencyLevel.ONE):
     simple_query = SimpleStatement(query, consistency_level=cl)
     res = cursor.execute(simple_query)
     list_res = rows_to_list(res)
-    assert list_res == [expected], "Expected %s from %s, but got %s" % (expected, query, list_res)
+    assert list_res == [expected], "Expected %s from %s, but got %s" % ([expected], query, list_res)
 
 def assert_none(cursor, query, cl=ConsistencyLevel.ONE):
     simple_query = SimpleStatement(query, consistency_level=cl)
