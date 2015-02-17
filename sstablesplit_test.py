87ef6ee515a83f14a6ab36240560005a44e9d91b
Philip Thompson <ptnapoleon@gmail.com>
2015-02-17 17:18:35 -0500

    Remove hack for windows

--- sstablesplit_test.py
+++ sstablesplit_test.py
@@ -12,15 +12,10 @@ class TestSSTableSplit(Tester):
         after carrying out these operations.
         """
         cluster = self.cluster
-        cluster.populate(1).start()
+        cluster.populate(1).start(wait_for_binary_proto=True)
         node = cluster.nodelist()[0]
         version = cluster.version()
 
-        # Here we need to wait to connect
-        # to the node. This is required for windows
-        # to prevent stress starting before the node
-        # is ready for connections
-        node.watch_log_for('thrift clients...')
         debug("Run stress to insert data")
         if version < "2.1":
             node.stress( ['-o', 'insert'] )
