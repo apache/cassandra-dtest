70ee21b2536c5cae3931b1786cd24eeb5342501e
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2015-03-09 17:16:39 +0800

    Removed unused code

--- write_failures_test.py
+++ write_failures_test.py
@@ -17,7 +17,6 @@ from tools import since
 from assertions import assert_one, assert_none
 
 KEYSPACE = "foo"
-COMMIT_LOG_ERROR = "ERROR \[COMMIT-LOG-ALLOCATOR\].+Failed .+ commit log segments"
 INVALID_MUTATION_ERROR = "Mutation failed with Mutation of"
 
 class TestWriteFailures(Tester):
@@ -29,7 +28,7 @@ class TestWriteFailures(Tester):
     def setUp(self):
         super(TestWriteFailures, self).setUp()
 
-        self.ignore_log_patterns = [ COMMIT_LOG_ERROR, INVALID_MUTATION_ERROR]
+        self.ignore_log_patterns = [ INVALID_MUTATION_ERROR]
 
         conf = {'commitlog_sync_period_in_ms': 1000}
         conf = {'commitlog_segment_size_in_mb': 1}
@@ -37,31 +36,11 @@ class TestWriteFailures(Tester):
         if self.cluster.version() >= "2.1":
             conf['memtable_heap_space_in_mb'] = 512
         
-        #conf['commit_failure_policy'] = 'ignore'
-
         self.cluster.set_configuration_options(values=conf)
         self.cluster.populate(3).start()
         
     def tearDown(self):
-        #self.restore_commitlog_perms(self.cluster.nodes.values()[1])
-        #self.restore_commitlog_perms(self.cluster.nodes.values()[2])
         super(TestWriteFailures, self).tearDown()
-
-    def change_commitlog_perms(self, node, mod):
-        path = os.path.join(node.get_path(), 'commitlogs')
-
-        debug("Setting mod %d for path %s, node %s" % (mod, path, self.get_ip_from_node(node)))
-        os.chmod(path, mod)
-
-        commitlogs = glob.glob(path+'/*')
-        for commitlog in commitlogs:
-            os.chmod(commitlog, mod)
-
-    def remove_commitlog_perms(self, node):
-        self.change_commitlog_perms(node, 0)
-
-    def restore_commitlog_perms(self, node):
-        self.change_commitlog_perms(node, stat.S_IWRITE | stat.S_IREAD | stat.S_IEXEC)
 
     def test_cql_mutation(self):
         """
@@ -69,11 +48,11 @@ class TestWriteFailures(Tester):
         for protocol version >= 4 and WriteTimeout for earlier versions.
         """
 
-        node1, node2, node3 = self.cluster.nodes.values()
+        node1 = self.cluster.nodes.values()[0]
 
         session = self.patient_cql_connection(node1)
-
         self.create_ks(session, KEYSPACE, 3)
+
         session.execute(
             """
                 CREATE TABLE users (
@@ -95,18 +74,3 @@ class TestWriteFailures(Tester):
 
         with self.assertRaises(WriteTimeout) as cm:
             session.execute(insert_statement)
-    
-
-        # An alternative is to fail the commit log by changing permissions
-        # but this unfortunately causes the mutation to wait rather than throw
-        # an exception
-        #self.remove_commitlog_perms(node2)
-        #self.remove_commitlog_perms(node3)
-
-        # At some point we should get our exception
-        #for i in range (0, 100000):
-        #    session.execute(insert_statement, (i, 0))
-        
-        #for node in (node2, node3):
-        #    failure = node.grep_log(COMMIT_LOG_ERROR)
-        #    self.assertTrue(failure, "Cannot find COMMIT_LOG_ERROR in logs of %s" % (self.get_ip_from_node(node)))
