2244c0e8f7e11388cedd9766d3a0514f0233a124
Ariel Weisberg <ariel@weisberg.ws>
2015-04-28 17:03:33 -0400

    For CASSANDRA-8670, address review comments

--- largecolumn_test.py
+++ largecolumn_test.py
@@ -2,6 +2,10 @@ from dtest import Tester, debug
 from tools import new_node, since
 import time
 
+"""
+Check that inserting and reading large columns to the database doesn't cause off heap memory usage
+that is proportional to the size of the memory read/written.
+"""
 @since('3.0')
 class TestLargeColumn(Tester):
 
@@ -18,19 +22,27 @@ class TestLargeColumn(Tester):
         See CASSANDRA-8670
         """
         cluster = self.cluster
+        #Commit log segment size needs to increase for the database to be willing to accept columns that large
+        #internode compression is disabled because the regression being tested occurs in NIO buffer pooling without compression
         cluster.set_configuration_options( { 'commitlog_segment_size_in_mb' : 128, 'internode_compression' : 'none' })
+        #Have Netty allocate memory on heap so it is clear if memory used for large columns is related to intracluster messaging
         cluster.populate(2).start(jvm_args=[" -Dcassandra.netty_use_heap_allocator=true "])
         node1, node2 = cluster.nodelist()
 
         session = self.patient_cql_connection(node1)
         debug("Before stress {0}".format(self.directbytes(node1)))
         debug("Running stress")
+        #Run the full stack to see how much memory is utilized for "small" columns
         self.stress_with_col_size(cluster, node1, 1)
         debug("Ran stress once {0}".format(self.directbytes(node1)))
 
+        #Now run the full stack to see how much memory is utilized for "large" columns
         self.stress_with_col_size(cluster, node1, 1024 * 1024 * 63)
 
         output = node1.nodetool( "gcstats", capture_output=True);
         afterStress = self.directbytes(node1)
         debug("After stress {0}".format(afterStress))
+
+        #Any growth in memory usage should not be proportional column size. Really almost no memory should be used
+        #since Netty was instructed to use a heap allocator
         assert int(afterStress) < 1024 * 1024 * 2, int(afterStress)
