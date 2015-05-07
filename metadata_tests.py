375bb9e49b5af85c0ce32640d9f9a7bb0c3dc581
Tyler Hobbs <tylerlhobbs@gmail.com>
2015-05-07 12:54:54 -0500

    Whitespace and import fixes

--- metadata_tests.py
+++ metadata_tests.py
@@ -1,15 +1,7 @@
-import random
-import subprocess
 import time
-import tempfile
 import threading
 
-from dtest import debug, Tester
-from tools import new_node, query_c1c2, since, InterruptBootstrap
-from assertions import assert_almost_equal
-from ccmlib.node import NodeError
-from cassandra import ConsistencyLevel
-from cassandra.concurrent import execute_concurrent_with_args
+from dtest import Tester
 
 
 class TestMetadata(Tester):
@@ -30,7 +22,6 @@ class TestMetadata(Tester):
         else:
             node1.nodetool('repair keyspace1 standard1')
 
-
     def do_read(self):
         cluster = self.cluster
         (node1, node2) = cluster.nodelist()
@@ -42,8 +33,9 @@ class TestMetadata(Tester):
                               '-rate', 'threads=1'])
 
     def metadata_reset_while_compact_test(self):
-        """ Resets the schema while a compact, read and repair happens. 
-            All kinds of glorious things can fail.
+        """
+        Resets the schema while a compact, read and repair happens.
+        All kinds of glorious things can fail.
         """
         cluster = self.cluster
         cluster.populate(2).start(wait_other_notice=True)
@@ -51,7 +43,7 @@ class TestMetadata(Tester):
 
         node1.nodetool("disableautocompaction")
         node1.nodetool("setcompactionthroughput 1")
-        
+
         for i in range(3):
             if cluster.version() < "2.1":
                 node1.stress(['-o', 'insert', '-n', '30000', '-l', '2', '-t', '5', '-I', 'LZ4Compressor'])
@@ -59,7 +51,7 @@ class TestMetadata(Tester):
                 node1.stress(['write', 'no-warmup', 'n=30000', '-schema', 'replication(factor=2)', 'compression=LZ4Compressor',
                               '-rate', 'threads=5', '-pop', 'seq=1..30000'])
             node1.flush()
-        
+
         thread = threading.Thread(target=self.force_compact)
         thread.start()
         time.sleep(1)
@@ -67,7 +59,7 @@ class TestMetadata(Tester):
         thread2 = threading.Thread(target=self.force_repair)
         thread2.start()
         time.sleep(5)
-        
+
         thread3 = threading.Thread(target=self.do_read)
         thread3.start()
         time.sleep(5)
@@ -77,4 +69,3 @@ class TestMetadata(Tester):
         thread.join()
         thread2.join()
         thread3.join()
-        
