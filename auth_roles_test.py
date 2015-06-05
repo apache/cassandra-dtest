59cdbb306a6a18d7de9bcc941364cf775fdfe972
Sam Tunnicliffe <sam@beobal.com>
2015-06-05 10:31:19 +0100

    Add test for CASSANDRA-9551

--- auth_roles_test.py
+++ auth_roles_test.py
@@ -1136,6 +1136,22 @@ class TestAuthRoles(Tester):
         # mike *does* have execute permission on the aggregate function, as its creator
         assert_one(mike, execute_aggregate_cql, [3])
 
+    def ignore_invalid_roles_test(self):
+        """
+        The system_auth.roles table includes a set of roles of which each role
+        is a member. If that list were to get out of sync, so that it indicated
+        that roleA is a member of roleB, but roleB does not exist in the roles
+        table, then the result of LIST ROLES OF roleA should not include roleB
+        @jira_ticket CASSANDRA-9551
+        """
+
+        self.prepare()
+        cassandra = self.get_session(user='cassandra', password='cassandra')
+        cassandra.execute("CREATE ROLE mike WITH LOGIN = true")
+        # hack an invalid entry into the roles table for roleA
+        cassandra.execute("UPDATE system_auth.roles SET member_of = {'role1'} where role = 'mike'")
+        assert_all(cassandra, "LIST ROLES OF mike", [mike_role])
+
     def setup_table(self, session):
         session.execute("CREATE KEYSPACE ks WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1}")
         session.execute("CREATE TABLE ks.t1 (k int PRIMARY KEY, v int)")
