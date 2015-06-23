9ba7158f3ccd83f2479937ccf1276996fa9fa2fe
Philip Thompson <ptnapoleon@gmail.com>
2015-06-23 13:05:20 -0400

    Wait for binary proto after upgrade

--- upgrade_through_versions_test.py
+++ upgrade_through_versions_test.py
@@ -274,7 +274,7 @@ class TestUpgradeThroughVersions(Tester):
             debug('Starting %s on new version (%s)' % (node.name, tag))
             # Setup log4j / logback again (necessary moving from 2.0 -> 2.1):
             node.set_log_level("INFO")
-            node.start(wait_other_notice=True)
+            node.start(wait_other_notice=True, wait_for_binary_proto=True)
             node.nodetool('upgradesstables -a')
 
     def _log_current_ver(self, current_tag):
