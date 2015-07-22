5e59b15c2b6d205817d8041606d6ab5e8a518dd0
Alan Boudreault <alan@alanb.ca>
2015-07-22 14:00:17 -0400

    Add linter_check.sh

---
+++ linter_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# linter_check ensures your changes will pass on travis.
+# Requires pep8 and flake8: pip install pep8 flake8
+
+flake8 --ignore=E,W,F811,F812,F821,F822,F823,F831,F841,N8,C9 --exclude=thrift_bindings,cassandra-thrift .
+flake8_result=$?
+
+git diff master -U0 | pep8 --diff --max-line-length=200
+pep8_result=$?
+
+echo -e "\nflake8 exited with ${flake8_result}."
+echo "pep8 exited with ${pep8_result}."
+
+if [ $flake8_result -ne 0 -o $pep8_result -ne 0 ];
+then
+    echo "Your changes contain linter errors."
+    exit 1
+fi
+
+echo "Done"
+exit 0
