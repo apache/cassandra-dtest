2246e42be901734aee35adf96268ab9817b9fa87
Philip Thompson <ptnapoleon@gmail.com>
2015-08-06 14:46:16 -0400

    PEP8 cleanup dtest.py

--- dtest.py
+++ dtest.py
@@ -1,18 +1,32 @@
 from __future__ import with_statement
-import os, tempfile, sys, shutil, subprocess, types, time, threading, traceback, ConfigParser, logging, re, copy
+
+import ConfigParser
+import copy
 import errno
+import logging
+import os
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+import threading
+import time
+import traceback
+import types
+from unittest import TestCase
+
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster as PyCluster
+from cassandra.cluster import NoHostAvailable
+from cassandra.policies import WhiteListRoundRobinPolicy
+from nose.exc import SkipTest
 
 from ccmlib.cluster import Cluster
 from ccmlib.cluster_factory import ClusterFactory
 from ccmlib.common import is_win
-from nose.exc import SkipTest
-from unittest import TestCase
-from cassandra.cluster import NoHostAvailable
-from cassandra.cluster import Cluster as PyCluster
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.policies import WhiteListRoundRobinPolicy
 
-LOG_SAVED_DIR="logs"
+LOG_SAVED_DIR = "logs"
 try:
     os.mkdir(LOG_SAVED_DIR)
 except OSError:
@@ -20,13 +34,13 @@ except OSError:
 
 LAST_LOG = os.path.join(LOG_SAVED_DIR, "last")
 
-LAST_TEST_DIR='last_test_dir'
+LAST_TEST_DIR = 'last_test_dir'
 
-DEFAULT_DIR='./'
+DEFAULT_DIR = './'
 config = ConfigParser.RawConfigParser()
 if len(config.read(os.path.expanduser('~/.cassandra-dtest'))) > 0:
     if config.has_option('main', 'default_dir'):
-        DEFAULT_DIR=os.path.expanduser(config.get('main', 'default_dir'))
+        DEFAULT_DIR = os.path.expanduser(config.get('main', 'default_dir'))
 CASSANDRA_DIR = os.environ.get('CASSANDRA_DIR', DEFAULT_DIR)
 
 NO_SKIP = os.environ.get('SKIP', '').lower() in ('no', 'false')
@@ -45,7 +59,7 @@ IGNORE_REQUIRE = os.environ.get('IGNORE_REQUIRE', '').lower() in ('yes', 'true')
 
 CURRENT_TEST = ""
 
-logging.basicConfig(filename=os.path.join(LOG_SAVED_DIR,"dtest.log"),
+logging.basicConfig(filename=os.path.join(LOG_SAVED_DIR, "dtest.log"),
                     filemode='w',
                     format='%(asctime)s,%(msecs)d %(name)s %(current_test)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
@@ -57,6 +71,8 @@ logging.getLogger('cassandra').setLevel(logging.WARNING)
 
 # copy the initial environment variables so we can reset them later:
 initial_environment = copy.deepcopy(os.environ)
+
+
 def reset_environment_vars():
     os.environ.clear()
     os.environ.update(initial_environment)
@@ -67,10 +83,12 @@ def warning(msg):
     if PRINT_DEBUG:
         print "WARN: " + msg
 
+
 def debug(msg):
-    LOG.debug(msg, extra={"current_test":CURRENT_TEST})
+    LOG.debug(msg, extra={"current_test": CURRENT_TEST})
     if PRINT_DEBUG:
         print msg
+
 
 def retry_till_success(fun, *args, **kwargs):
     timeout = kwargs.pop('timeout', 60)
@@ -86,6 +104,7 @@ def retry_till_success(fun, *args, **kwargs):
             else:
                 # brief pause before next attempt
                 time.sleep(0.25)
+
 
 class Runner(threading.Thread):
     def __init__(self, func):
@@ -135,7 +154,7 @@ class Tester(TestCase):
         # ccm on cygwin needs absolute path to directory - it crosses from cygwin space into
         # regular Windows space on wmic calls which will otherwise break pathing
         if sys.platform == "cygwin":
-            self.test_path = subprocess.Popen(["cygpath", "-m", self.test_path], stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0].rstrip()
+            self.test_path = subprocess.Popen(["cygpath", "-m", self.test_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].rstrip()
         debug("cluster ccm directory: "+self.test_path)
         version = os.environ.get('CASSANDRA_VERSION')
         cdir = CASSANDRA_DIR
@@ -263,11 +282,11 @@ class Tester(TestCase):
             self.cluster.set_configuration_options(values=self.cluster_options)
         else:
             self.cluster.set_configuration_options(values={
-                'read_request_timeout_in_ms' : timeout,
-                'range_request_timeout_in_ms' : timeout,
-                'write_request_timeout_in_ms' : timeout,
-                'truncate_request_timeout_in_ms' : timeout,
-                'request_timeout_in_ms' : timeout
+                'read_request_timeout_in_ms': timeout,
+                'range_request_timeout_in_ms': timeout,
+                'write_request_timeout_in_ms': timeout,
+                'truncate_request_timeout_in_ms': timeout,
+                'request_timeout_in_ms': timeout
             })
 
         with open(LAST_TEST_DIR, 'w') as f:
@@ -288,7 +307,7 @@ class Tester(TestCase):
             name = os.path.join(directory, name)
         if not os.path.exists(directory):
             os.mkdir(directory)
-        logs = [ (node.name, node.logfilename()) for node in self.cluster.nodes.values() ]
+        logs = [(node.name, node.logfilename()) for node in self.cluster.nodes.values()]
         if len(logs) is not 0:
             basedir = str(int(time.time() * 1000)) + '_' + self.id()
             logdir = os.path.join(directory, basedir)
@@ -399,7 +418,7 @@ class Tester(TestCase):
         else:
             assert len(rf) != 0, "At least one datacenter/rf pair is needed"
             # we assume networkTopolyStrategy
-            options = (', ').join([ '\'%s\':%d' % (d, r) for d, r in rf.iteritems() ])
+            options = (', ').join(['\'%s\':%d' % (d, r) for d, r in rf.iteritems()])
             session.execute(query % (name, "'class':'NetworkTopologyStrategy', %s" % options))
         session.execute('USE %s' % name)
 
@@ -436,7 +455,6 @@ class Tester(TestCase):
 
         session.execute(query)
         time.sleep(0.2)
-
 
     @classmethod
     def tearDownClass(cls):
@@ -518,7 +536,7 @@ class Tester(TestCase):
         if os.path.isfile(agent_location):
             debug("Jacoco agent found at {}".format(agent_location))
             with open(os.path.join(
-                    self.test_path, cluster_name, 'cassandra.in.sh'),'w') as f:
+                    self.test_path, cluster_name, 'cassandra.in.sh'), 'w') as f:
 
                 f.write('JVM_OPTS="$JVM_OPTS -javaagent:{jar_path}=destfile={exec_file}"'\
                     .format(jar_path=agent_location, exec_file=jacoco_execfile))
@@ -556,12 +574,13 @@ class Tester(TestCase):
 
     def make_auth(self, user, password):
         def private_auth(node_ip):
-            return {'username': user, 'password' : password}
+            return {'username': user, 'password': password}
         return private_auth
 
     # Disable docstrings printing in nosetest output
     def shortDescription(self):
         return None
+
 
 def canReuseCluster(Tester):
     orig_init = Tester.__init__
@@ -569,9 +588,9 @@ def canReuseCluster(Tester):
 
     def __init__(self, *args, **kwargs):
         self._preserve_cluster = REUSE_CLUSTER
-        orig_init(self, *args, **kwargs) # call the original __init__
+        orig_init(self, *args, **kwargs)  # call the original __init__
 
-    Tester.__init__ = __init__ # set the class' __init__ to the new one
+    Tester.__init__ = __init__  # set the class' __init__ to the new one
     return Tester
 
 
