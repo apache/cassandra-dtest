3707bbc6b34dabc33b6a223fd4fb7fbf37e7d23e
Robert Stupp <snazy@snazy.de>
2015-10-23 12:32:15 +0200

    dtest for CASSANDRA-10559

--- native_transport_ssl_test.py
+++ native_transport_ssl_test.py
@@ -35,6 +35,23 @@ class NativeTransportSSL(Tester):
         session = self.patient_cql_connection(node1, ssl_opts={'ca_certs': os.path.join(self.test_path, 'ccm_node.cer')})
         self._putget(cluster, session)
 
+    @since('3.2')
+    def connect_to_ssl_optional_test(self):
+        """
+        Connecting to SSL optional native transport port must be possible with SSL and non-SSL native clients
+        """
+        cluster = self._populateCluster(enableSSL=True, sslOptional=True)
+        node1 = cluster.nodelist()[0]
+
+        # try to connect without ssl options
+        cluster.start()
+        session = self.patient_cql_connection(node1)
+        self._putget(cluster, session)
+
+        # enabled ssl on the client and try again (this should work)
+        session = self.patient_cql_connection(node1, ssl_opts={'ca_certs': os.path.join(self.test_path, 'ccm_node.cer')})
+        self._putget(cluster, session, ks='ks2')
+
     def use_custom_port_test(self):
         """
         Connect to non-default native transport port
@@ -72,18 +89,30 @@ class NativeTransportSSL(Tester):
         session = self.patient_cql_connection(node1, port=9666, ssl_opts={'ca_certs': os.path.join(self.test_path, 'ccm_node.cer')})
         self._putget(cluster, session, ks='ks2')
 
-    def _populateCluster(self, enableSSL=False, nativePort=None, nativePortSSL=None):
+    def _populateCluster(self, enableSSL=False, nativePort=None, nativePortSSL=None, sslOptional=False):
         cluster = self.cluster
 
         if enableSSL:
             generate_ssl_stores(self.test_path)
-            cluster.set_configuration_options({
-                'client_encryption_options': {
-                    'enabled': True,
-                    'keystore': os.path.join(self.test_path, 'keystore.jks'),
-                    'keystore_password': 'cassandra'
-                }
-            })
+            # C* versions before 3.2 (with CASSANDRA-10559) do not know about
+            # 'client_encryption_options.optional' - so we must not add that parameter
+            if sslOptional:
+                cluster.set_configuration_options({
+                    'client_encryption_options': {
+                        'enabled': True,
+                        'optional': sslOptional,
+                        'keystore': os.path.join(self.test_path, 'keystore.jks'),
+                        'keystore_password': 'cassandra'
+                    }
+                })
+            else:
+                cluster.set_configuration_options({
+                    'client_encryption_options': {
+                        'enabled': True,
+                        'keystore': os.path.join(self.test_path, 'keystore.jks'),
+                        'keystore_password': 'cassandra'
+                    }
+                })
 
         if nativePort:
             cluster.set_configuration_options({
