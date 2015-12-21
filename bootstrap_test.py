546e878e86f36edfcc4a2f0cd6a989a28d807df4
Jim Witschey <jim.witschey@gmail.com>
2015-12-21 16:36:07 -0500

    annotate failure for 10912

--- bootstrap_test.py
+++ bootstrap_test.py
@@ -109,6 +109,9 @@ class TestBootstrap(Tester):
         self.assertEquals(original_rows, new_rows)
 
     @since('2.2')
+    @known_failure(failure_source='systemic',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-10912',
+                   flaky=True)
     def resumable_bootstrap_test(self):
         """Test resuming bootstrap after data streaming failure"""
 
