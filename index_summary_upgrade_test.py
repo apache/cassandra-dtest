45854f7f262c30d8a041f625e45f196887b7fbf6
Jim Witschey <jim.witschey@gmail.com>
2015-12-21 22:07:58 -0500

    unrequire index_summary_upgrade_test

--- index_summary_upgrade_test.py
+++ index_summary_upgrade_test.py
@@ -1,5 +1,4 @@
 from dtest import Tester, debug
-from tools import require
 
 from cassandra.concurrent import execute_concurrent_with_args
 
@@ -8,7 +7,6 @@ from jmxutils import JolokiaAgent, make_mbean, remove_perf_disable_shared_mem
 
 class TestUpgradeIndexSummary(Tester):
 
-    @require('Fixes to ccm around upgrades')
     def test_upgrade_index_summary(self):
         cluster = self.cluster
         cluster.populate(1)
