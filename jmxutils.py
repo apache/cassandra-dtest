45573c90a042ccd2c8063f01d86a8ebad83b5624
Philip Thompson <ptnapoleon@gmail.com>
2015-12-30 12:27:43 -0500

    Revert "Totally undo changes for 10494. Revert once that is committed."
    This reverts commit 5bfc5937c931998918a62a896a7bbc575c4d439c.

--- jmxutils.py
+++ jmxutils.py
@@ -57,12 +57,17 @@ def remove_perf_disable_shared_mem(node):
     option (see https://github.com/rhuss/jolokia/issues/198 for details).  This
     edits cassandra-env.sh (or the Windows equivalent), or jvm.options file on 3.2+ to remove that option.
     """
-    if common.is_win():
-        conf_file = os.path.join(node.get_conf_dir(), common.CASSANDRA_WIN_ENV)
+    if node.cluster.version() >= '3.2':
+        conf_file = os.path.join(node.get_conf_dir(), JVM_OPTIONS)
+        pattern = '\-XX:\+PerfDisableSharedMem'
+        replacement = '#-XX:+PerfDisableSharedMem'
     else:
-        conf_file = os.path.join(node.get_conf_dir(), common.CASSANDRA_ENV)
-    pattern = 'PerfDisableSharedMem'
-    replacement = ''
+        if common.is_win():
+            conf_file = os.path.join(node.get_conf_dir(), common.CASSANDRA_WIN_ENV)
+        else:
+            conf_file = os.path.join(node.get_conf_dir(), common.CASSANDRA_ENV)
+        pattern = 'PerfDisableSharedMem'
+        replacement = ''
 
     common.replace_in_file(conf_file, pattern, replacement)
 
