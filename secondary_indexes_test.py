bf114879dc678b2acd73bb94e81d8d313bf8b243
Philip Thompson <ptnapoleon@gmail.com>
2016-02-04 14:01:42 -0500

    Remove 10905 annotation.

--- secondary_indexes_test.py
+++ secondary_indexes_test.py
@@ -5,7 +5,7 @@ import time
 import uuid
 
 from dtest import Tester, debug
-from tools import known_failure, since, rows_to_list
+from tools import since, rows_to_list
 from assertions import assert_invalid, assert_one
 from cassandra import InvalidRequest
 from cassandra.concurrent import execute_concurrent, execute_concurrent_with_args
@@ -341,9 +341,6 @@ class TestSecondaryIndexes(Tester):
         rows = list(session.execute("SELECT * FROM tbl WHERE c0 = 'a' AND c1 = 'b' ALLOW FILTERING;"))
         self.assertEqual(2, len(rows))
 
-    @known_failure(failure_source='cassandra',
-                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-10905',
-                   flaky=True)
     @since('3.0')
     def test_only_coordinator_chooses_index_for_query(self):
         """
