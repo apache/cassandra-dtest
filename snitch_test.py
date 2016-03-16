c3cf0ad70f7d512e94a7c44638b3449c70a8085f
Philip Thompson <ptnapoleon@gmail.com>
2016-03-16 16:38:38 -0400

    Add failure annotation for 11362

--- snitch_test.py
+++ snitch_test.py
@@ -2,7 +2,7 @@ import os
 import socket
 
 from dtest import Tester, debug
-from tools import since
+from tools import since, known_failure
 
 
 @since('2.2.5')
@@ -15,6 +15,9 @@ class TestGossipingPropertyFileSnitch(Tester):
         s.connect((address, port))
         s.close()
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11362',
+                   flaky=True)
     def test_prefer_local_reconnect_on_listen_address(self):
         """
         @jira_ticket CASSANDRA-9748
