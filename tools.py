977c7311d8ce6e56da8bebaa0312c45f71f0253e
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-04-20 13:56:52 +0800

    CASSANDRA-11497: Fixed SSTableUtilTest.abortedcompaction_test

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import fileinput
 import functools
 import os
+import random
 import re
 import subprocess
 import sys
@@ -415,15 +416,21 @@ class InterruptCompaction(Thread):
     available in a file called "debug.log" unless a
     different name is passed in as a paramter.
     """
-    def __init__(self, node, tablename, filename='debug.log'):
+    def __init__(self, node, tablename, filename='debug.log', delay=0):
         Thread.__init__(self)
         self.node = node
         self.tablename = tablename
         self.filename = filename
+        self.delay = delay
         self.mark = node.mark_log(filename=self.filename)
 
     def run(self):
         self.node.watch_log_for("Compacting(.*)%s" % (self.tablename,), from_mark=self.mark, filename=self.filename)
+        if self.delay > 0:
+            random_delay = random.uniform(0, self.delay)
+            debug("Sleeping for {} seconds".format(random_delay))
+            time.sleep(random_delay)
+        debug("Killing node {}".format(self.node.address()))
         self.node.stop(gently=False)
 
 
