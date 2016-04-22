6d24d889d0f45b94451538c94937cea3ac932a24
Russ Hatch <russthehatch@gmail.com>
2016-04-22 12:44:57 -0600

    unknown CASSANDRA-11499

--- commitlog_test.py
+++ commitlog_test.py
@@ -160,9 +160,6 @@ class TestCommitLog(Tester):
         with open(os.devnull, 'w') as devnull:
             self.node1.stress(['write', 'n=1M', '-col', 'size=FIXED(1000)', '-rate', 'threads=25'], stdout=devnull, stderr=subprocess.STDOUT)
 
-    @known_failure(failure_source='test',
-                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11499',
-                   flaky=True)
     def test_commitlog_replay_on_startup(self):
         """
         Test commit log replay
