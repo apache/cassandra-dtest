be33445ca76af9595231015526773fca9bcb6fe9
Russ Hatch <russthehatch@gmail.com>
2016-05-13 10:29:06 -0600

    upcase override_manifest, and check as bool

--- upgrade_tests/upgrade_manifest.py
+++ upgrade_tests/upgrade_manifest.py
@@ -65,14 +65,14 @@ MANIFEST = {
 
 # Local env and custom path testing instructions. Use these steps to REPLACE the normal upgrade test cases with your own.
 # 1) Add a VersionMeta for each version you wish to test (see examples below). Update the name, version, and protocol restrictions as needed. Use a unique name for each VersionMeta.
-# 2) Update override_manifest (see example below).
+# 2) Update OVERRIDE_MANIFEST (see example below).
 # 3) If using ccm local: slugs, make sure you have LOCAL_GIT_REPO defined in your env. This is the path to your git repo.
 # 4) Run the tests! To run all, use 'nosetests -v upgrade_tests/'. To run specific tests, use 'nosetests -vs --collect-only' to preview the test names, then run nosetests using the desired test name.
 custom_1 = VersionMeta(name='custom_branch_1', variant='indev', version='local:some_branch', min_proto_v=3, max_proto_v=4)
 custom_2 = VersionMeta(name='custom_branch_2', variant='indev', version='git:trunk', min_proto_v=3, max_proto_v=4)
 custom_3 = VersionMeta(name='custom_branch_3', variant='indev', version='git:cassandra-3.5', min_proto_v=3, max_proto_v=4)
 custom_4 = VersionMeta(name='custom_branch_4', variant='indev', version='git:cassandra-3.6', min_proto_v=3, max_proto_v=4)
-override_manifest = {
+OVERRIDE_MANIFEST = {
     # EXAMPLE:
     # custom_1: [custom_2, custom_3],  # creates a test of custom_1 -> custom_2, and another test from custom_1 -> custom_3
     # custom_3: [custom_4]             # creates a test of custom_3 -> custom_4
@@ -97,7 +97,7 @@ def _is_targeted_variant_combo(origin_meta, destination_meta):
       current -> next (aka: released -> proposed release point)
       next -> in-dev (aka: proposed release point -> branch)
     """
-    return (override_manifest or  # if we're overriding the test manifest, we don't want to filter anything out
+    return (bool(OVERRIDE_MANIFEST) or  # if we're overriding the test manifest, we don't want to filter anything out
             (origin_meta.variant == 'current' and destination_meta.variant == 'indev') or
             (origin_meta.variant == 'current' and destination_meta.variant == 'next') or
             (origin_meta.variant == 'next' and destination_meta.variant == 'indev'))
@@ -110,7 +110,7 @@ def build_upgrade_pairs():
     Returns a list of UpgradePath's.
     """
     valid_upgrade_pairs = []
-    manifest = override_manifest or MANIFEST
+    manifest = OVERRIDE_MANIFEST or MANIFEST
 
     for origin_meta, destination_metas in manifest.items():
         for destination_meta in destination_metas:
