b1d4d166c7c7c3bcfa84cd86ec59fb9f2c90d5f7
Philip Thompson <ptnapoleon@gmail.com>
2016-05-17 13:38:38 -0400

    Re-order annotations because the notes field was being confusing

--- replication_test.py
+++ replication_test.py
@@ -391,12 +391,12 @@ class SnitchConfigurationUpdateTest(Tester):
                                        nodes_to_shutdown=[0, 2])
 
     @known_failure(failure_source='test',
-                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11690',
-                   flaky=True)
-    @known_failure(failure_source='test',
                    jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11439',
                    flaky=False,
                    notes='Windows')
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11690',
+                   flaky=True)
     def test_rf_collapse_gossiping_property_file_snitch_multi_dc(self):
         """
         @jira_ticket CASSANDRA-10238
