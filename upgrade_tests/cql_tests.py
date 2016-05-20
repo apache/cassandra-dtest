3ac3a78aae11ed80525c7e2ff23329cafa698dc3
Philip Thompson <ptnapoleon@gmail.com>
2016-05-20 13:36:42 -0400

    Add 11858 annotation

--- upgrade_tests/cql_tests.py
+++ upgrade_tests/cql_tests.py
@@ -3817,6 +3817,10 @@ class TestCQL(UpgradeTester):
             cursor.execute("DELETE s FROM test WHERE k=0")
             assert_all(cursor, "SELECT * FROM test", [[0, 1, None, 1]])
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11858',
+                   flaky=False,
+                   notes='Fails on 2.0 to 2.1 upgrade')
     def static_columns_cas_test(self):
         cursor = self.prepare()
 
