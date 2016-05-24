93aa0cba3a138cd0b6d52cd35c6e6edf65308b8f
Philip Thompson <ptnapoleon@gmail.com>
2016-05-24 16:25:38 -0400

    Remove unused import

--- thrift_tests.py
+++ thrift_tests.py
@@ -21,7 +21,7 @@ from thrift_bindings.v22.Cassandra import (CfDef, Column, ColumnDef,
                                            Mutation, NotFoundException,
                                            SlicePredicate, SliceRange,
                                            SuperColumn)
-from tools import known_failure, since
+from tools import since
 
 
 def get_thrift_client(host='127.0.0.1', port=9160):
