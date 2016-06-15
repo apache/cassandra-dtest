fd893110d3b0702d07b8553c835aaffc90012170
Paulo Motta <pauloricardomg@gmail.com>
2016-06-14 23:39:15 -0300

    Add sstableloader test with pre-3.0 sstables

--- sstable_generation_loading_test.py
+++ sstable_generation_loading_test.py
@@ -6,96 +6,30 @@ from distutils import dir_util
 from ccmlib import common as ccmcommon
 
 from dtest import Tester, debug
-from tools import known_failure
+from tools import since, known_failure
+
+LEGACY_SSTABLES_JVM_ARGS = ["-Dcassandra.streamdes.initial_mem_buffer_size=1",
+                            "-Dcassandra.streamdes.max_mem_buffer_size=5",
+                            "-Dcassandra.streamdes.max_spill_file_size=64"]
 
 
-class TestSSTableGenerationAndLoading(Tester):
+class BaseSStableLoaderTest(Tester):
+    __test__ = False
+    upgrade_from = None
+    compact = False
+    jvm_args = None
 
     def __init__(self, *argv, **kwargs):
-        kwargs['cluster_options'] = {'start_rpc': 'true'}
-        super(TestSSTableGenerationAndLoading, self).__init__(*argv, **kwargs)
+        if self.jvm_args is None:
+            self.jvm_args = []
+        kwargs['cluster_options'] = {'start_rpc': True}
+        Tester.__init__(self, *argv, **kwargs)
         self.allow_log_errors = True
 
     def create_schema(self, session, ks, compression):
         self.create_ks(session, ks, rf=2)
         self.create_cf(session, "standard1", compression=compression)
         self.create_cf(session, "counter1", compression=compression, columns={'v': 'counter'})
-
-    def incompressible_data_in_compressed_table_test(self):
-        """
-        tests for the bug that caused #3370:
-        https://issues.apache.org/jira/browse/CASSANDRA-3370
-
-        @jira_ticket CASSANDRA-3370
-
-        inserts random data into a compressed table. The compressed SSTable was
-        compared to the uncompressed and was found to indeed be larger then
-        uncompressed.
-        """
-        cluster = self.cluster
-        cluster.populate(1).start()
-        node1 = cluster.nodelist()[0]
-        time.sleep(.5)
-
-        session = self.patient_cql_connection(node1)
-        self.create_ks(session, 'ks', 1)
-        self.create_cf(session, 'cf', compression="Deflate")
-
-        # make unique column names, and values that are incompressible
-        for col in xrange(10):
-            col_name = str(col)
-            col_val = os.urandom(5000)
-            col_val = col_val.encode('hex')
-            cql = "UPDATE cf SET v='%s' WHERE KEY='0' AND c='%s'" % (col_val, col_name)
-            # print cql
-            session.execute(cql)
-
-        node1.flush()
-        time.sleep(2)
-        rows = list(session.execute("SELECT * FROM cf WHERE KEY = '0' AND c < '8'"))
-        self.assertGreater(len(rows), 0)
-
-    def remove_index_file_test(self):
-        """
-        tests for situations similar to that found in #343:
-        https://issues.apache.org/jira/browse/CASSANDRA-343
-
-        @jira_ticket CASSANDRA-343
-        """
-        cluster = self.cluster
-        cluster.populate(1).start(wait_for_binary_proto=True)
-        node1 = cluster.nodelist()[0]
-
-        # Makinge sure the cluster is ready to accept the subsequent
-        # stress connection. This was an issue on Windows.
-        node1.stress(['write', 'n=10K', 'no-warmup', '-rate', 'threads=8'])
-        node1.flush()
-        node1.compact()
-        node1.stop()
-        time.sleep(1)
-        paths = []
-        for data_dir in node1.data_directories():
-            basepath = os.path.join(data_dir, 'keyspace1')
-            for x in os.listdir(basepath):
-                if x.startswith("standard1"):
-                    path = os.path.join(basepath, x)
-
-            os.system('rm %s/*Index.db' % path)
-            os.system('rm %s/*Filter.db' % path)
-            os.system('rm %s/*Statistics.db' % path)
-            os.system('rm %s/*Digest.sha1' % path)
-            paths.append(path)
-
-        node1.start()
-
-        time.sleep(10)
-
-        data_found = 0
-        for path in paths:
-            for fname in os.listdir(path):
-                if fname.endswith('Data.db'):
-                    data_found += 1
-        self.assertGreater(data_found, 0, "After removing index, filter, stats, and digest files, the data file was deleted!")
 
     @known_failure(failure_source='test',
                    jira_url='https://issues.apache.org/jira/browse/CASSANDRA-11896',
@@ -175,9 +109,17 @@ class TestSSTableGenerationAndLoading(Tester):
             assert compression_option in (None, 'Snappy', 'Deflate')
 
         debug("Testing sstableloader with pre_compression=%s and post_compression=%s" % (pre_compression, post_compression))
+        if self.upgrade_from:
+            debug("Testing sstableloader with upgrade_from=%s and compact=%s" % (self.upgrade_from, self.compact))
 
         cluster = self.cluster
-        cluster.populate(2).start()
+        if self.upgrade_from:
+            debug("Generating sstables with version %s" % (self.upgrade_from))
+            default_install_dir = self.cluster.get_install_dir()
+            # Forcing cluster version on purpose
+            cluster.set_install_dir(version=self.upgrade_from)
+        debug("Using jvm_args=%s" % self.jvm_args)
+        cluster.populate(2).start(jvm_args=self.jvm_args)
         node1, node2 = cluster.nodelist()
         time.sleep(.5)
 
@@ -208,7 +150,13 @@ class TestSSTableGenerationAndLoading(Tester):
         debug("Wiping out the data and restarting cluster")
         # wipe out the node data.
         cluster.clear()
-        cluster.start()
+
+        if self.upgrade_from:
+            debug("Running sstableloader with version from %s" % (default_install_dir))
+            # Return to previous version
+            cluster.set_install_dir(install_dir=default_install_dir)
+
+        cluster.start(jvm_args=self.jvm_args)
         time.sleep(5)  # let gossip figure out what is going on
 
         debug("re-creating the keyspace and column families.")
@@ -253,3 +201,136 @@ class TestSSTableGenerationAndLoading(Tester):
 
         debug("Reading data back one more time")
         read_and_validate_data(session)
+
+        # check that RewindableDataInputStreamPlus spill files are properly cleaned up
+        if self.upgrade_from:
+            for x in xrange(0, cluster.data_dir_count):
+                data_dir = os.path.join(node1.get_path(), 'data{0}'.format(x))
+                for ddir in os.listdir(data_dir):
+                    keyspace_dir = os.path.join(data_dir, ddir)
+                    temp_files = self.glob_data_dirs(os.path.join(keyspace_dir, '*', "tmp", "*.dat"))
+                    debug("temp files: " + str(temp_files))
+                    self.assertEquals(0, len(temp_files), "Temporary files were not cleaned up.")
+
+
+@since('3.0')
+class TestLoadKaSStables(BaseSStableLoaderTest):
+    __test__ = True
+    upgrade_from = '2.1.6'
+    jvm_args = LEGACY_SSTABLES_JVM_ARGS
+
+
+@since('3.0')
+class TestLoadKaCompactSStables(BaseSStableLoaderTest):
+    __test__ = True
+    upgrade_from = '2.1.6'
+    jvm_args = LEGACY_SSTABLES_JVM_ARGS
+    compact = True
+
+
+@since('3.0')
+class TestLoadLaSStables(BaseSStableLoaderTest):
+    __test__ = True
+    upgrade_from = '2.2.4'
+    jvm_args = LEGACY_SSTABLES_JVM_ARGS
+
+
+@since('3.0')
+class TestLoadLaCompactSStables(BaseSStableLoaderTest):
+    __test__ = True
+    upgrade_from = '2.2.4'
+    jvm_args = LEGACY_SSTABLES_JVM_ARGS
+    compact = True
+
+
+class TestSSTableGenerationAndLoading(BaseSStableLoaderTest):
+    __test__ = True
+
+    def incompressible_data_in_compressed_table_test(self):
+        """
+        tests for the bug that caused #3370:
+        https://issues.apache.org/jira/browse/CASSANDRA-3370
+
+        @jira_ticket CASSANDRA-3370
+
+        inserts random data into a compressed table. The compressed SSTable was
+        compared to the uncompressed and was found to indeed be larger then
+        uncompressed.
+        """
+        cluster = self.cluster
+        cluster.populate(1).start()
+        node1 = cluster.nodelist()[0]
+        time.sleep(.5)
+
+        session = self.patient_cql_connection(node1)
+        self.create_ks(session, 'ks', 1)
+        self.create_cf(session, 'cf', compression="Deflate")
+
+        # make unique column names, and values that are incompressible
+        for col in xrange(10):
+            col_name = str(col)
+            col_val = os.urandom(5000)
+            col_val = col_val.encode('hex')
+            cql = "UPDATE cf SET v='%s' WHERE KEY='0' AND c='%s'" % (col_val, col_name)
+            # print cql
+            session.execute(cql)
+
+        node1.flush()
+        time.sleep(2)
+        rows = list(session.execute("SELECT * FROM cf WHERE KEY = '0' AND c < '8'"))
+        self.assertGreater(len(rows), 0)
+
+    def remove_index_file_test(self):
+        """
+        tests for situations similar to that found in #343:
+        https://issues.apache.org/jira/browse/CASSANDRA-343
+
+        @jira_ticket CASSANDRA-343
+        """
+        cluster = self.cluster
+        cluster.populate(1).start(wait_for_binary_proto=True)
+        node1 = cluster.nodelist()[0]
+
+        # Makinge sure the cluster is ready to accept the subsequent
+        # stress connection. This was an issue on Windows.
+        node1.stress(['write', 'n=10K', 'no-warmup', '-rate', 'threads=8'])
+        node1.flush()
+        node1.compact()
+        node1.stop()
+        time.sleep(1)
+        paths = []
+        for data_dir in node1.data_directories():
+            basepath = os.path.join(data_dir, 'keyspace1')
+            for x in os.listdir(basepath):
+                if x.startswith("standard1"):
+                    path = os.path.join(basepath, x)
+
+            os.system('rm %s/*Index.db' % path)
+            os.system('rm %s/*Filter.db' % path)
+            os.system('rm %s/*Statistics.db' % path)
+            os.system('rm %s/*Digest.sha1' % path)
+            paths.append(path)
+
+        node1.start()
+
+        time.sleep(10)
+
+        data_found = 0
+        for path in paths:
+            for fname in os.listdir(path):
+                if fname.endswith('Data.db'):
+                    data_found += 1
+        self.assertGreater(data_found, 0, "After removing index, filter, stats, and digest files, the data file was deleted!")
+
+    def sstableloader_with_mv_test(self):
+        """
+        @jira_ticket CASSANDRA-11275
+        """
+        def create_schema_with_mv(session, ks, compression):
+            self.create_schema(session, ks, compression)
+            # create a materialized view
+            session.execute("CREATE MATERIALIZED VIEW mv1 AS "
+                            "SELECT key FROM standard1 WHERE key IS NOT NULL AND c IS NOT NULL AND v IS NOT NULL "
+                            "PRIMARY KEY (v)")
+
+        self.load_sstable_with_configuration(ks='"Keyspace1"', create_schema=create_schema_with_mv)
