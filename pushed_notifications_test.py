d332edd549627b176c6f08265b39cb4d1683a42d
Sam Tunnicliffe <sam@beobal.com>
2016-06-22 19:37:40 +0100

    Only run test from CASSANDRA-11038 on 2.2+

--- pushed_notifications_test.py
+++ pushed_notifications_test.py
@@ -217,6 +217,7 @@ class TestPushedNotifications(Tester):
         notifications = waiter.wait_for_notifications(timeout=30.0, num_notifications=2)
         self.assertEquals(0, len(notifications), notifications)
 
+    @since("2.2")
     def add_and_remove_node_test(self):
         """
         Test that NEW_NODE and REMOVED_NODE are sent correctly as nodes join and leave.
