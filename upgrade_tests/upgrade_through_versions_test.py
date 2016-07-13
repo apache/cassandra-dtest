1c920e4d29d579fcef9a8081530ac60ce7b0ee20
Sean McCarthy <spm64@pitt.edu>
2016-07-13 13:16:56 -0400

    annotations for upgrade_test_all 59 failures

--- upgrade_tests/upgrade_through_versions_test.py
+++ upgrade_tests/upgrade_through_versions_test.py
@@ -288,6 +288,9 @@ class UpgradeTester(Tester):
         """
         self.upgrade_scenario(internode_ssl=True)
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12195',
+                   flaky=False)
     def rolling_upgrade_with_internode_ssl_test(self):
         """
         Rolling upgrade test using internode ssl.
@@ -702,6 +705,9 @@ class BootstrapMixin(object):
         self._check_values()
         self._check_counters()
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12196',
+                   flaky=False)
     def bootstrap_test(self):
         # try and add a new node
         self.upgrade_scenario(after_upgrade_call=(self._bootstrap_new_node,))
