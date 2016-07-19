425d28b9ae3b3b2d682d1249775fe28ce493639e
Jim Witschey <jim.witschey@gmail.com>
2016-07-19 14:52:15 -0400

    use more permissive retry policy for insertion

--- repair_tests/repair_test.py
+++ repair_tests/repair_test.py
@@ -6,7 +6,7 @@ from unittest import skip
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement
 
-from dtest import Tester, debug
+from dtest import Tester, debug, FlakyRetryPolicy
 from tools import insert_c1c2, known_failure, no_vnodes, query_c1c2, since
 
 
@@ -90,6 +90,7 @@ class BaseRepairTest(Tester):
         node1, node2, node3 = cluster.nodelist()
 
         session = self.patient_cql_connection(node1)
+        session.cluster.default_retry_policy = FlakyRetryPolicy(max_retries=15)
         self.create_ks(session, 'ks', 3)
         self.create_cf(session, 'cf', read_repair=0.0, columns={'c1': 'text', 'c2': 'text'})
 
