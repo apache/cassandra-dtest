f71c036d640e04c10e9fa608c440f828e0edf6d6
Philip Thompson <ptnapoleon@gmail.com>
2016-07-20 11:54:11 -0400

    Add 12250 annotation

--- upgrade_tests/cql_tests.py
+++ upgrade_tests/cql_tests.py
@@ -5169,6 +5169,9 @@ class TestCQL(UpgradeTester):
 
             assert_none(cursor, "select * from space1.table1 where a=1 and b=1")
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12250',
+                   flaky=True)
     def bug_5732_test(self):
         cursor = self.prepare(use_cache=True)
 
