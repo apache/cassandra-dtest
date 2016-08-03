dfa91f93176c9f24afb3fae1e507cc0ce44dbf35
Josh McKenzie <jmckenzie@apache.org>
2016-08-03 13:30:25 -0400

    CASSANDRA-12148 cdc hard-link support

--- cqlsh_tests/cqlsh_tools.py
+++ cqlsh_tests/cqlsh_tools.py
@@ -3,6 +3,10 @@ import random
 
 import cassandra
 
+from cassandra.cluster import ResultSet
+from nose.tools import assert_true
+from typing import List
+
 
 class DummyColorMap(object):
 
@@ -89,3 +93,29 @@ def unmonkeypatch_driver(cache):
 
     if hasattr(cassandra, 'deserializers'):
         cassandra.deserializers.DesDateType = cache['DesDateType']
+
+
+def assert_resultset_contains(got: ResultSet, expected: List[tuple]) -> None:
+    """
+    So this is slow. I would hope a ResultSet has the capability of pulling data by PK or clustering,
+    however I'm not finding it atm. As such, this method isn't intended for use with large datasets.
+    :param got: ResultSet, expect schema of [a, b]
+    :param expected: list of tuples with 2 members corresponding with a/b schema of ResultSet
+    """
+    # Adding a touch of sanity check so people don't mis-use this. n^2 is bad.
+    assert len(expected) <= 1000, 'This is a slow comparison method. Don\'t use for > 1000 tuples.'
+
+    # First quick check: if we have a different count, we can just die.
+    assert_true(len(got.current_rows) == len(expected))
+
+    for t in expected:
+        assert len(t) == 2, 'Got unexpected tuple len. Expected 2, got tuple: {}'.format(t)
+        found = False
+        for row in got.current_rows:
+            if found:
+                break
+            if row.a == t[0] and row.b == t[1]:
+                found = True
+        assert_true(found, 'Failed to find expected row: {}'.format(t))
+
+
