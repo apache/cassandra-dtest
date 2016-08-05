658848a491fdd0f0b57f751782f8cb1f1dbd99bf
c-kodman <ckoddrums@gmail.com>
2016-08-05 12:49:41 -0400

    add known_failure annotation (#1181)

--- repair_tests/repair_test.py
+++ repair_tests/repair_test.py
@@ -742,6 +742,9 @@ class TestRepair(BaseRepairTest):
                           rows[0][0],
                           'Expected {} job threads in repair options. Instead we saw {}'.format(job_thread_count, rows[0][0]))
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12395',
+                   flaky=False)
     @no_vnodes()
     def test_multiple_concurrent_repairs(self):
         """
