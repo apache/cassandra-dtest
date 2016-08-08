1d3061735b2383f211f95c2883173a5aa9055075
Philip Thompson <ptnapoleon@gmail.com>
2016-08-08 14:08:33 -0400

    Remove 12285 annotation

--- user_functions_test.py
+++ user_functions_test.py
@@ -6,7 +6,7 @@ from cassandra import FunctionFailure
 
 from assertions import assert_invalid, assert_none, assert_one
 from dtest import CASSANDRA_VERSION_FROM_BUILD, Tester, debug
-from tools import since, known_failure
+from tools import since
 
 
 @since('2.2')
@@ -32,9 +32,6 @@ class TestUserFunctions(Tester):
             self.create_ks(session, 'ks', rf)
         return session
 
-    @known_failure(failure_source='test',
-                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12285',
-                   flaky=False)
     def test_migration(self):
         """ Test migration of user functions """
         cluster = self.cluster
