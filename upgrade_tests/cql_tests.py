6ee684641633eb6d2a15ff659753f7e12d2b9a8d
c-kodman <ckoddrums@gmail.com>
2016-08-16 11:00:41 -0400

    add known_failure annotation (#1231)

--- upgrade_tests/cql_tests.py
+++ upgrade_tests/cql_tests.py
@@ -5195,6 +5195,9 @@ class TestCQL(UpgradeTester):
 
             assert_none(cursor, "select * from space1.table1 where a=1 and b=1")
 
+    @known_failure(failure_source='test',
+                   jira_url='https://issues.apache.org/jira/browse/CASSANDRA-12457',
+                   flaky=True)
     def bug_5732_test(self):
         cursor = self.prepare(use_cache=True)
 
