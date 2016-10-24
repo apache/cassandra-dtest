56b83d49e252fa5f72c139314e612c60acdf0c16
Philip Thompson <ptnapoleon@gmail.com>
2016-10-24 15:23:27 -0400

    Remove unused import

--- cqlsh_tests/cqlsh_copy_tests.py
+++ cqlsh_tests/cqlsh_copy_tests.py
@@ -27,7 +27,7 @@ from cqlsh_tools import (DummyColorMap, assert_csvs_items_equal, csv_rows,
 from dtest import (DISABLE_VNODES, Tester, canReuseCluster, debug,
                    freshCluster, warning, create_ks)
 from tools.data import rows_to_list
-from tools.decorators import known_failure, since
+from tools.decorators import since
 from tools.metadata_wrapper import (UpdatingClusterMetadataWrapper,
                                     UpdatingTableMetadataWrapper)
 
