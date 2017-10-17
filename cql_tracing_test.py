edc48bc965e842628413cfd50a7a21071d7b098a
Sam Tunnicliffe <sam@beobal.com>
2017-10-17 14:50:25 +0100

    Add test for digest requests with RandomPartitioner and tracing enabled
    Patch by Sam Tunnicliffe; reviewed by Jason Brown for CASSANDRA-13964

--- cql_tracing_test.py
+++ cql_tracing_test.py
@@ -3,6 +3,7 @@ from distutils.version import LooseVersion
 
 from dtest import Tester, debug, create_ks
 from tools.decorators import since
+from tools.jmxutils import make_mbean, JolokiaAgent, remove_perf_disable_shared_mem
 
 
 class TestCqlTracing(Tester):
@@ -15,16 +16,23 @@ class TestCqlTracing(Tester):
     #      instantiated when specified as a custom tracing implementation.
     """
 
-    def prepare(self, create_keyspace=True, nodes=3, rf=3, protocol_version=3, jvm_args=None, **kwargs):
+    def prepare(self, create_keyspace=True, nodes=3, rf=3, protocol_version=3, jvm_args=None, random_partitioner=False, **kwargs):
         if jvm_args is None:
             jvm_args = []
 
         jvm_args.append('-Dcassandra.wait_for_tracing_events_timeout_secs=15')
 
         cluster = self.cluster
-        cluster.populate(nodes).start(wait_for_binary_proto=True, jvm_args=jvm_args)
 
+        if random_partitioner:
+            cluster.set_partitioner("org.apache.cassandra.dht.RandomPartitioner")
+        else:
+            cluster.set_partitioner("org.apache.cassandra.dht.Murmur3Partitioner")
+
+        cluster.populate(nodes)
         node1 = cluster.nodelist()[0]
+        remove_perf_disable_shared_mem(node1)  # necessary for jmx
+        cluster.start(wait_for_binary_proto=True, jvm_args=jvm_args)
 
         session = self.patient_cql_connection(node1, protocol_version=protocol_version)
         if create_keyspace:
@@ -176,3 +184,30 @@ class TestCqlTracing(Tester):
         self.assertIn("Default constructor for Tracing class "
                       "'org.apache.cassandra.tracing.TracingImpl' is inaccessible.",
                       check_for_errs_in)
+
+    @since('3.0')
+    def test_tracing_does_not_interfere_with_digest_calculation(self):
+        """
+        Test that enabling tracing doesn't interfere with digest responses when using RandomPartitioner.
+        The use of a threadlocal MessageDigest for generating both DigestResponse messages and for
+        calculating tokens meant that the DigestResponse was always incorrect when both RP and tracing
+        were enabled, leading to unnecessary data reads.
+
+        @jira_ticket CASSANDRA-13964
+        """
+
+        session = self.prepare(random_partitioner=True)
+        self.trace(session)
+
+        node1 = self.cluster.nodelist()[0]
+
+        rr_count = make_mbean('metrics', type='ReadRepair', name='RepairedBlocking')
+        with JolokiaAgent(node1) as jmx:
+            # the MBean may not have been initialized, in which case Jolokia agent will return
+            # a HTTP 404 response. If we receive such, we know that no digest mismatch was reported
+            # If we are able to read the MBean attribute, assert that the count is 0
+            if jmx.has_mbean(rr_count):
+                # expect 0 digest mismatches
+                self.assertEqual(0, jmx.read_attribute(rr_count, 'Count'))
+            else:
+                pass
