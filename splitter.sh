85b3e2bba8df31de4de9f40214d973ffb7fd6300
Bereng <berenguerblasi@gmail.com>
2021-09-15 08:01:27 +0200

    splitter debu of reuse renew files

--- splitter.sh
+++ splitter.sh
@@ -33,6 +33,12 @@ then
 else
   linesRenew=`wc -l $renewableTestsFile | cut -d " " -f 1`
 fi
+
+echo REUSABLE FILE
+cat $reusableTestsFile
+echo RENEWABLE FILE
+cat $renewableTestsFile
+
 totalTests=`expr $linesReuse + $linesRenew`
 testsPerSplit=`expr $totalTests / $splits`
 echo "Reuse cluster tests: " $linesReuse
