6fa64f00e491075d225927f61f66a02ff559bc85
Bereng <berenguerblasi@gmail.com>
2021-10-15 10:13:20 +0200

    Splitter that gives reuse splits twice the number of tests

--- splitter.sh
+++ splitter.sh
@@ -8,43 +8,38 @@ reusableTestsFile=$3
 renewableTestsFile=$4
 outFile=$5
 
-if [ -z "$chunk" ] || [ -z "$splits" ] || [ -z "$reusableTestsFile" ] || [ -z "$renewableTestsFile" ] || [ -z "$outFile" ]
-then
+if [ -z "$chunk" ] || [ -z "$splits" ] || [ -z "$reusableTestsFile" ] || [ -z "$renewableTestsFile" ] || [ -z "$outFile" ]; then
   echo "Usage: splitter.sh <chunk> <splits> <reusableTestsFile> <renewableTestsFile> <outFile>"
   exit 1
 fi
 
-if [ `grep -f $reusableTestsFile $renewableTestsFile | wc -l` -gt 0 ]
-then
+if [ `grep -f $reusableTestsFile $renewableTestsFile | wc -l` -gt 0 ]; then
   echo "There are tests that are in both reuse and renew files. That sould be impossible"
   exit 1
 fi
 
 #Get the numbers per no/reusage
-if [ ! -f $reusableTestsFile ]
-then
+if [ ! -f $reusableTestsFile ]; then
   linesReuse=0
 else
   linesReuse=`wc -l $reusableTestsFile | cut -d " " -f 1`
 fi
-if [ ! -f $renewableTestsFile ]
-then
+if [ ! -f $renewableTestsFile ]; then
   linesRenew=0
 else
   linesRenew=`wc -l $renewableTestsFile | cut -d " " -f 1`
 fi
 
-totalTests=`expr $linesReuse + $linesRenew`
-testsPerSplit=`expr $totalTests / $splits`
+totalTests=$(( $linesReuse + $linesRenew ))
+testsPerSplit=$(( $totalTests / $splits ))
 echo "Reuse cluster tests: " $linesReuse
 echo "No reuse cluster tests: " $linesRenew
 echo "Total tests: " $totalTests
 echo "Splits: " $splits " Chunk: " $chunk
-echo "Tests per split: " $testsPerSplit
 
 #How many splits per no/reussage of cluster?
-reuseClusterSplits=`expr $linesReuse / $testsPerSplit`
-renewClusterSplits=`expr $splits - $reuseClusterSplits`
+reuseClusterSplits=$(( $linesReuse / $testsPerSplit / 2 )) #Reuse run twice as much tests
+renewClusterSplits=$(( $splits - $reuseClusterSplits ))
 echo "Reuse cluster splits: " $reuseClusterSplits
 echo "Renew cluster splits: " $renewClusterSplits
 
@@ -52,13 +47,11 @@ echo "Renew cluster splits: " $renewClusterSplits
 split -n r/$renewClusterSplits --suffix-length=6 --numeric-suffixes=1 --additional-suffix=testSplitsRR $renewableTestsFile
 rm renewClusterTestsRR.txt
 awk 1 x*testSplitsRR >> renewClusterTestsRR.txt
-
-#Put reusable tests first and the RR non-reusable later. awk needed bc sometimes some file had no newline at the end
-awk 1 $reusableTestsFile > test_list_arranged.txt
-awk 1 renewClusterTestsRR.txt >> test_list_arranged.txt
 rm x*testSplitsRR
 
-split -n l/$splits --suffix-length=6 --numeric-suffixes=1 --additional-suffix=testSplits test_list_arranged.txt
+split -n l/$reuseClusterSplits --suffix-length=6 --numeric-suffixes=1 --additional-suffix=testSplits $reusableTestsFile
+split -n l/$renewClusterSplits --suffix-length=6 --numeric-suffixes=$(( $reuseClusterSplits + 1 )) --additional-suffix=testSplits renewClusterTestsRR.txt
+
 printf -v formattedChunk "%06d" $chunk
 mySplitFile="x${formattedChunk}testSplits"
 echo "My split is: " $mySplitFile
