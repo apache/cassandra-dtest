0e48973aa0c93437ecd2a9d016a2f8b9b950f74d
Bereng <berenguerblasi@gmail.com>
2021-10-19 07:04:22 +0200

    Reusable class level splitter

--- splitter.sh
+++ splitter.sh
@@ -18,43 +18,16 @@ if [ `grep -f $reusableTestsFile $renewableTestsFile | wc -l` -gt 0 ]; then
   exit 1
 fi
 
-#Get the numbers per no/reusage
-if [ ! -f $reusableTestsFile ]; then
-  linesReuse=0
+# Extract test classes from reusable tests list
+cat  $reusableTestsFile | sed s/[^:]*$// | sed s/::$// | uniq > reuseTestClasses.txt
+
+awk 1 reuseTestClasses.txt > combinedTests.txt
+awk 1 $renewableTestsFile >> combinedTests.txt
+
+if [ "$CIRCLECI" = true ]; then
+  circleci tests split --split-by=timings --timings-type=classname combinedTests.txt > $outFile
 else
-  linesReuse=`wc -l $reusableTestsFile | cut -d " " -f 1`
-fi
-if [ ! -f $renewableTestsFile ]; then
-  linesRenew=0
-else
-  linesRenew=`wc -l $renewableTestsFile | cut -d " " -f 1`
+  #Split tests with round robin to spread weight of heavy test classes
+  split -n r/$chunk combinedTests.txt > $outFile
 fi
 
-totalTests=$(( $linesReuse + $linesRenew ))
-testsPerSplit=$(( $totalTests / $splits ))
-echo "Reuse cluster tests: " $linesReuse
-echo "No reuse cluster tests: " $linesRenew
-echo "Total tests: " $totalTests
-echo "Splits: " $splits " Chunk: " $chunk
-
-#How many splits per no/reussage of cluster?
-reuseClusterSplits=$(( $linesReuse / $testsPerSplit / 2 )) #Reuse run twice as much tests
-renewClusterSplits=$(( $splits - $reuseClusterSplits ))
-echo "Reuse cluster splits: " $reuseClusterSplits
-echo "Renew cluster splits: " $renewClusterSplits
-
-#Split non reusable cluster tests with round robin to spread weight of heavy test classes
-split -n r/$renewClusterSplits --suffix-length=6 --numeric-suffixes=1 --additional-suffix=testSplitsRR $renewableTestsFile
-rm renewClusterTestsRR.txt
-awk 1 x*testSplitsRR >> renewClusterTestsRR.txt
-rm x*testSplitsRR
-
-split -n l/$reuseClusterSplits --suffix-length=6 --numeric-suffixes=1 --additional-suffix=testSplits $reusableTestsFile
-split -n l/$renewClusterSplits --suffix-length=6 --numeric-suffixes=$(( $reuseClusterSplits + 1 )) --additional-suffix=testSplits renewClusterTestsRR.txt
-
-printf -v formattedChunk "%06d" $chunk
-mySplitFile="x${formattedChunk}testSplits"
-echo "My split is: " $mySplitFile
-cp $mySplitFile $outFile
-
-rm renewClusterTestsRR.txt
