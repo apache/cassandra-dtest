64e528aa123bc6d8e736e835eb6e9d0c32ff0953
Stefan Miklosovic <smiklosovic@apache.org>
2026-03-23 14:04:29 +0100

    ninja: fix accord-core lib regexp

--- read_repair_test.py
+++ read_repair_test.py
@@ -38,7 +38,7 @@ def byteman_validate(node, script, verbose=False, opts=None):
     ]
 
     if os.path.exists(os.path.join(cdir, 'modules', 'accord')):
-        jars.append(glob.glob(os.path.join(cdir, 'modules', 'accord', 'accord-core', 'build', 'libs', 'accord-core-[0-9].[0-9]-SNAPSHOT.jar'))[0])
+        jars.append(glob.glob(os.path.join(cdir, 'modules', 'accord', 'accord-core', 'build', 'libs', 'accord-core-*-SNAPSHOT.jar'))[0])
 
     byteman_cmd.append(':'.join(jars))
     byteman_cmd.append('org.jboss.byteman.check.TestScript')
